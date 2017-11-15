9bb43ced4d4047b226125a7544bb76946886a03d
Erick Damian <erickdlp95@gmail.com>
2017-11-15 01:26:03 -0600

    initial commit

---
+++ src/footer.vue
@@ -0,0 +1,38 @@
+<<template>
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+        <!--<div class="columns">
+            <div class="column">
+                <button class=""></button>
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column">
+
+            </div>
+            <div class="column">
+
+            </div>
+        </div>-->
+        <div class="field is-grouped">
+            <a class="button" href="#">Fb</a>
+            <a class="button" href="#">Twitter</a>
+            <a class="button" href="#">Gmail</a>
+            <a class="button" href="#">LinkedIn</a>
+        </div>
+    </div>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import footer from './footer.vue'
+import bulma from 'bulma/css/bulma.css'
+
+new Vue({
+    el: '#app',
+    components:{
+        footer
+    }
+})
